3988fb834ed42cbb8f265e0a6ae74fd1ad025656
jeremy <bensimon.jeremy@gmail.com>
2019-04-10 14:01:19 +0200

    feat: launch chrome with empty user dir, enable developer mode in Preferences file, update to extension page on start

---
+++ __fixture__/webpack.config.js
@@ -0,0 +1,34 @@
+const { resolve } = require('path')
+const Copy = require('copy-webpack-plugin')
+const ChromeExtensionLauncher = require('../src/main')
+
+const src = (path = '') => resolve(__dirname, 'src', path)
+const dist = resolve(__dirname, 'dist')
+
+/** @return {import('webpack').Configuration} */
+module.exports = (env, argv) => {
+	const isProd = env === 'prod' || Boolean(env && env.prod)
+	const isDev = !isProd
+
+	return {
+		entry: {
+			background: src('background.js'),
+			'content-script': src('content-script.js'),
+			popup: src('popup.js'),
+			options: src('options.js'),
+		},
+		output: {
+			filename: '[name].js',
+			path: dist,
+		},
+		resolve: { extensions: ['.js'] },
+		mode: isProd ? 'production' : 'development',
+		devtool: isDev ? 'inline-source-map' : false,
+		watch: isDev,
+		plugins: [
+			new ChromeExtensionLauncher(),
+			// @ts-ignore
+			new Copy([src('options.html'), src('popup.html'), src('manifest.json')]),
+		],
+	}
+}
---
+++ src/main.js
@@ -0,0 +1,121 @@
+const { exec } = require('child_process')
+const { tmpdir } = require('os')
+const { resolve } = require('path')
+const { mkdirSync, existsSync, readFileSync, writeFileSync } = require('fs')
+const InjectPlugin = require('webpack-inject-plugin').default
+const get = require('lodash.get')
+const set = require('lodash.set')
+const chrome = require('chrome-location')
+
+class ChromeExtensionLauncher {
+	/** @param {{path?: string; entry?: string}} options */
+	constructor(options = {}) {
+		this.path = options.path || '' // output.path by default
+		this.entry = options.entry || 'background'
+		this.launched = false
+	}
+
+	/** @param {import('webpack').Compiler} compiler */
+	apply(compiler) {
+		// Dont do anything in production mode
+		if (compiler.options.mode === 'production') {
+			return console.warn('[Chrome Extension Launcher] Production mode: disabled')
+		}
+
+		compiler.hooks.afterEmit.tapAsync('webpack-chrome-extension-launcher', (compilation, callback) => {
+			// Launch only one instance
+			if (this.launched) return callback()
+
+			const path = this.path || compilation.outputOptions.path
+			const userDataDir = this.createTempUserDataDir()
+			this.launchChrome(path, userDataDir)
+			this.enableDeveloperMode(userDataDir)
+			this.launched = true
+			callback()
+		})
+
+		// Inject code to open extensions page on startup
+		// (we can't open chrome protocol urls any other way)
+		new InjectPlugin(() => `chrome.tabs.update({ url: 'chrome://extensions/' })`, {
+			entryName: this.entry,
+		}).apply(compiler)
+	}
+
+	/**
+	 * Create or reuse new chrome user folder
+	 */
+	createTempUserDataDir() {
+		const userDataDir = resolve(tmpdir(), 'webpack-chrome-extension-launcher')
+
+		try {
+			mkdirSync(userDataDir)
+		} catch (error) {
+			if (error.code === 'EEXIST') {
+				console.log('[Chrome Extension Launcher] Using existing temporary user profile')
+			} else {
+				throw error
+			}
+		}
+
+		return userDataDir
+	}
+
+	/**
+	 * Open a new Chrome instance (with an new user profile) loaded with the unpacked extension
+	 * @param {string} extension
+	 * @param {string} userDataDir
+	 */
+	launchChrome(extension, userDataDir) {
+		console.log('[Chrome Extension Launcher] Launching Chrome instance')
+
+		return new Promise((resolve) => {
+			// https://peter.sh/experiments/chromium-command-line-switches/
+			const child = exec(
+				`"${chrome}" --load-extension="${extension}" --user-data-dir="${userDataDir}" --auto-open-devtools-for-tabs`,
+				(error) => {
+					if (error) throw error
+					resolve()
+				}
+			)
+			child.stdout.pipe(process.stdout)
+			child.stderr.pipe(process.stderr)
+		})
+	}
+
+	/**
+	 * Enables developer mode for extensions
+	 * @param {string} usrDataDir
+	 */
+	enableDeveloperMode(usrDataDir) {
+		const prefsFile = resolve(usrDataDir, 'Default', 'Preferences')
+
+		return this.waitForFileEvery(prefsFile, 10).then(() => {
+			const prefs = JSON.parse(readFileSync(prefsFile, 'utf8'))
+
+			const developerMode = get(prefs, 'extensions.ui.developer_mode')
+			if (developerMode === true) return
+
+			console.log('[Chrome Extension Launcher] Enabling developer mode')
+			set(prefs, 'extensions.ui.developer_mode', true)
+			writeFileSync(prefsFile, JSON.stringify(prefs), 'utf8')
+		})
+	}
+
+	/**
+	 * Wait for created file
+	 * @param {string} file
+	 * @param {number} ms
+	 */
+	waitForFileEvery(file, ms) {
+		return new Promise((resolve) => {
+			const timer = setInterval(() => {
+				if (existsSync(file)) {
+					clearInterval(timer)
+					resolve()
+				}
+			}, ms)
+		})
+	}
+}
+
+exports = module.exports = ChromeExtensionLauncher
